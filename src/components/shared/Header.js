0a7db2b07e9fe054db5f9ecc7fe93e947175e448
Rupam Dhara <rupamdhara.1579@gmail.com>
2025-05-12 07:27:50 +0530

    Updates

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -2,9 +2,7 @@ import DetectLocation from "../ui/DetectLoction";
 
 const Header = () => {
     return (
-        <header className="px-3 pt-4">
-            <p className="font-semibold">Guest</p>
-            <span className="text-gray-500">Select Location</span>
+        <header className="px-3 pt-4 header">
             <DetectLocation />
         </header>
     )
